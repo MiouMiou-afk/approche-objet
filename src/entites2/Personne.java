47f7fcb4e93681837bddc4536520186479b74f56
Myriam <myriam@cinetwork.app>
2022-10-10 17:35:30 +0200

    addt tp 03- instance et methode

--- src/entites2/Personne.java
+++ src/entites2/Personne.java
@@ -6,13 +6,42 @@ public class Personne {
 
 	public String name;
 	public String firstName;
-	public AdressePostale adresse ;
-	
-	public Personne(String name, String firstName, AdressePostale adresse ) {
-		this.name = name;
+	public AdressePostale adresse;
+
+	public Personne(String name, String firstName, AdressePostale adresse) {
+		this.name = name.toUpperCase();
 		this.firstName = firstName;
 		this.adresse = adresse;
 	}
+
+	public void setName(String name) {
+
+		this.name = name;
+	}
+
+	public void setFirstName(String firstName) {
+
+		this.firstName = firstName;
+	}
+
+	public void setAdresse(AdressePostale adresse) {
+
+		this.adresse = adresse;
+	}
 	
-	
+	public String getName() {
+
+		return name;
+	}
+
+	public String getFirstName() {
+
+		return  firstName;
+	}
+
+	public AdressePostale getAdresse() {
+
+		return  adresse;
+	}
+
 }
--- src/entities/TestPersonne.java
+++ src/entities/TestPersonne.java
@@ -6,14 +6,17 @@ public class TestPersonne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		Personne pers1 = new Personne("Salma", "Hayekoum",  new AdressePostale(22, "rue de Jva", 13006, "Marseille"));
-	
-		
-		Personne pers2 = new Personne("toto", "lolo", new AdressePostale(22, "rue de Jva", 13006, "Marseille" ));
-      
-		
-		System.out.println(pers2);
+
+		Personne pers1 = new Personne("Salma", "Hayekoum", new AdressePostale(22, "rue de Java", 13006, "Marseille"));
+		String namePers = pers1.getName();
+		String firstNamePers = pers1.getFirstName();
+		System.out.println("le pers1 se nomme " + namePers + " " + firstNamePers);
+
+		Personne pers2 = new Personne("toto", "lolo", new AdressePostale(22, "rue de Jva", 13006, "Marseille"));
+		pers2.setAdresse(new AdressePostale(25, "rue de la paix", 34200, "Mont"));
+
+		System.out.println("l'adresse est bien modifié : " + pers2.getAdresse().numeroRue +" " +  pers2.getAdresse().libelleRue+" " +  pers2.getAdresse().codePostal+" " +  pers2.getAdresse().ville);
+
 	}
 
 }
